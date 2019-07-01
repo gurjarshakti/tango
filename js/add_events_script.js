b9e543e8f911e4ec3618f41d6986d8434a50401a
gurjarshakti <mrshaktisingh33@gmail.com>
2019-07-01 11:56:26 +0530

    Latest changes

--- js/add_events_script.js
+++ js/add_events_script.js
@@ -50,33 +50,3 @@ extraFileFive.onclick = function() {
 }
 
 // ------------- 24 June End---------------
-
-// ------------- 25 June Start -----------
-
-// let info = document.getElementsByClassName('modal-body');
-// let myTab = document.getElementById('view-table');
-
-// info.innerHTML = '';
-
-// function showTableData() {
-    
-// 	// LOOP THROUGH EACH ROW OF THE TABLE AFTER HEADER.
-//     for (let i = 1; i < myTab.rows.length; i++) {
-
-//      	// GET THE CELLS COLLECTION OF THE CURRENT ROW.
-//         let objCells = myTab.rows.item(i).cells;
-
-// 	}
-
-// // LOOP THROUGH EACH CELL OF THE CURENT ROW TO READ CELL VALUES.
-//     for (let j = 0; j < objCells.length; j++) {
-
-//         info.innerHTML = info.innerHTML + 'Your Event Booking Details are as follows: ' + objCells.item(j).innerHTML;
-
-//     }
-
-//     info.innerHTML = info.innerHTML + '<br />';
-
-// }
-
-// ------------- 25 June End -------------
